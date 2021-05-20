a1bb63202b6a17ead2dc7210f315afa2165dc662
Ceren BATTAL <cebattal@netas.com.tr>
2021-05-20 21:48:34 +0300

    create poll component is done

--- src/components/poll/Answer.js
+++ src/components/poll/Answer.js
@@ -4,17 +4,9 @@ import './Answer.css'
 export default function Answer() {
     return (
         <div className="answer-form">
-            <div class="fluid ui two column stackable grid container">
-                <div class="column">
-                    <div class="ui form">
-                        <div class="field">
-                            <input placeholder="Answer" type="text" />
-                        </div>
-                    </div>
-                </div>
-                <div class="column">
-                    <div class="ui button">Add</div>
-                </div>
+            <div class="ui fluid icon input">
+                <input type="text" placeholder="Search..." />
+                <i class="circular plus link icon"></i>
             </div>
         </div>
     )
--- src/components/poll/CreatePoll.js
+++ src/components/poll/CreatePoll.js
@@ -9,6 +9,11 @@ export default function CreatePoll() {
                 <input type="text" placeholder="Type a question..." />
             </div>
             <Answer />
+            <div class="footer">
+                <div class="small ui right floated button">Create</div>
+                <div class="small ui right floated button">Reset</div>
+                <p>0/10 possible answers</p>
+            </div>
         </div>
     )
 }
